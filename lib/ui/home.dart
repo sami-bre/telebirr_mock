1981535ccc68517be286380b4fee459075499ccb
sami-bre <samuelbirhanu121@gmail.com>
2023-03-15 08:15:35 +0300

    now the layout for the 'balance row' is complete. Things are put where they're supposed to be. But it remains to work on the colors.

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -57,68 +57,134 @@ class _WalletScreenState extends State<WalletScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Switch(
-              value: _isDarkMode,
-              onChanged: (bool value) {
-                _isDarkMode
-                    ? Get.changeThemeMode(ThemeMode.light)
-                    : Get.changeThemeMode(ThemeMode.dark);
-                setState(() {
-                  _isDarkMode = !_isDarkMode;
-                });
-              },
-            ),
-            const Text("Hello User!")
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            buildBalanceView()
-          ],
-        )
-      ],
+    return SingleChildScrollView(
+      padding: EdgeInsets.symmetric(
+        horizontal: MediaQuery.of(context).size.width * 0.04,
+        vertical: 0,
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Switch(
+                value: _isDarkMode,
+                onChanged: (bool value) {
+                  _isDarkMode
+                      ? Get.changeThemeMode(ThemeMode.light)
+                      : Get.changeThemeMode(ThemeMode.dark);
+                  setState(() {
+                    _isDarkMode = !_isDarkMode;
+                  });
+                },
+              ),
+              const Text("Hello User!")
+            ],
+          ),
+          buildBalanceRow(),
+          SizedBox(height: 2000)
+        ],
+      ),
     );
   }
 
-  Container buildBalanceView() {
+  SizedBox buildBalanceRow() {
+    return SizedBox(
+      height: 168,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                height: 74,
+                width: 74,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Text("Pay"),
+                ),
+              ),
+              const Spacer(),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                height: 74,
+                width: 74,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Icon(Icons.barcode_reader),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(width: 20),
+          Expanded(
+            child: buildBalanceView(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildBalanceView() {
     return Container(
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.blue[800]!, width: 2),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: GestureDetector(
-              onTap: revealBallance,
-              child: TweenAnimationBuilder<double>(
-                duration: const Duration(milliseconds: 700),
-                tween: Tween(begin: 7, end: _balanceBlurValue),
-                curve: Curves.ease,
-                builder: (context, value, child) {
-                  return Blur(
-                    blur: value,
-                    borderRadius: BorderRadius.circular(10),
-                    overlay: _balanceBlurValue != 0
-                        ? const Text("tap to reveal balance")
-                        : null,
-                    child: const Padding(
-                      padding: EdgeInsets.all(12.0),
-                      child: Text(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.blue[800]!, width: 2),
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: GestureDetector(
+        onTap: revealBallance,
+        child: TweenAnimationBuilder<double>(
+          duration: const Duration(milliseconds: 700),
+          tween: Tween(begin: 7, end: _balanceBlurValue),
+          curve: Curves.ease,
+          builder: (context, value, child) {
+            return Blur(
+              blur: value,
+              borderRadius: BorderRadius.circular(30),
+              overlay: _balanceBlurValue != 0
+                  ? const Text("tap to reveal balance")
+                  : null,
+              child: Container(
+                padding: const EdgeInsets.all(12.0),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
                         "hey there",
                         style: TextStyle(
                           fontSize: 36,
                         ),
                       ),
-                    ),
-                  );
-                },
+                    ],
+                  ),
+                ),
               ),
-            ),
-          );
+            );
+          },
+        ),
+      ),
+    );
   }
 
   void revealBallance() {
