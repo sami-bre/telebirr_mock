c9889e26240738e98cc9c535435ad69faa585335
sami-bre <samuelbirhanu121@gmail.com>
2023-03-15 11:00:12 +0300

    now the the 'info-row' is fully layed out. But it remains to do the theming

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,4 +1,5 @@
 import 'package:blur/blur.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -54,6 +55,15 @@ class WalletScreen extends StatefulWidget {
 class _WalletScreenState extends State<WalletScreen> {
   bool _isDarkMode = Get.isDarkMode;
   double _balanceBlurValue = 7;
+  final daysList = const <Widget>[
+    Text("Mo"),
+    Text("Tu"),
+    Text("We"),
+    Text("Th"),
+    Text("Fr"),
+    Text("Sa"),
+    Text("Su"),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -82,7 +92,126 @@ class _WalletScreenState extends State<WalletScreen> {
             ],
           ),
           buildBalanceRow(),
+          const SizedBox(height: 20),
+          buildInfoRow(context),
           SizedBox(height: 2000)
+        ],
+      ),
+    );
+  }
+
+  Padding buildInfoRow(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: const [
+                Spacer(),
+                CircleAvatar(
+                  backgroundColor: Colors.blue,
+                  radius: 8,
+                ),
+                SizedBox(width: 5),
+                Text("Accepted"),
+                Spacer(),
+                CircleAvatar(
+                  backgroundColor: Colors.red,
+                  radius: 8,
+                ),
+                SizedBox(width: 5),
+                Text("Payed"),
+                Spacer(),
+              ],
+            ),
+          ),
+          AspectRatio(
+            aspectRatio: 2,
+            child: TweenAnimationBuilder(
+              duration: const Duration(milliseconds: 1500),
+              tween: Tween<double>(begin: 0, end: 1),
+              curve: Curves.ease,
+              builder: (context, value, child) {
+                return BarChart(
+                  BarChartData(
+                    barGroups: <BarChartGroupData>[
+                      BarChartGroupData(x: 0, barRods: <BarChartRodData>[
+                        BarChartRodData(toY: 700 * value, color: Colors.blue),
+                        BarChartRodData(toY: 420 * value, color: Colors.red)
+                      ]),
+                      BarChartGroupData(x: 1, barRods: <BarChartRodData>[
+                        BarChartRodData(toY: 562, color: Colors.blue),
+                        BarChartRodData(toY: 910 * value, color: Colors.red)
+                      ]),
+                      BarChartGroupData(x: 2, barRods: <BarChartRodData>[
+                        BarChartRodData(toY: 1157 * value, color: Colors.blue),
+                        BarChartRodData(toY: 225, color: Colors.red)
+                      ]),
+                      BarChartGroupData(x: 3, barRods: <BarChartRodData>[
+                        BarChartRodData(toY: 496 * value, color: Colors.blue),
+                        BarChartRodData(toY: 841 * value, color: Colors.red)
+                      ]),
+                      BarChartGroupData(x: 4, barRods: <BarChartRodData>[
+                        BarChartRodData(toY: 1254 * value, color: Colors.blue),
+                        BarChartRodData(toY: 594, color: Colors.red)
+                      ]),
+                      BarChartGroupData(x: 5, barRods: <BarChartRodData>[
+                        BarChartRodData(toY: 270 * value, color: Colors.blue),
+                        BarChartRodData(toY: 1008 * value, color: Colors.red)
+                      ]),
+                      BarChartGroupData(x: 6, barRods: <BarChartRodData>[
+                        BarChartRodData(toY: 417 * value, color: Colors.blue),
+                        BarChartRodData(toY: 827 * value, color: Colors.red)
+                      ]),
+                    ],
+                    borderData: FlBorderData(show: false),
+                    gridData: FlGridData(show: false),
+                    titlesData: FlTitlesData(
+                        bottomTitles: AxisTitles(
+                          sideTitles: SideTitles(
+                            showTitles: true,
+                            getTitlesWidget: (value, meta) =>
+                                daysList[value.toInt()],
+                          ),
+                        ),
+                        rightTitles: AxisTitles(
+                          sideTitles: SideTitles(showTitles: false),
+                        ),
+                        topTitles: AxisTitles(
+                            sideTitles: SideTitles(showTitles: false))),
+                  ),
+                );
+              },
+            ),
+          ),
+          const SizedBox(height: 5),
+          Row(
+            children: [
+              const Spacer(),
+              Text(
+                "Transactions",
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+              const SizedBox(width: 30),
+              SizedBox(
+                width: 100,
+                height: 46,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Text("More"),
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
